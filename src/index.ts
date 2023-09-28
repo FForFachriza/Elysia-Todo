2aaf997e12fc84391925e78c7d78ca67078062a6
FForFachriza <dimasavrian346@gmail.com>
2023-09-28 18:35:37 +0700

    [+] Init

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
+import { swagger } from "@elysiajs/swagger";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .use(swagger())
+  .guard({
+    response: t.String(),
+  })
+  .get("/", () => "1")
+  .get("/ping", () => "pong")
+  .get("/id/:id", ({ params: { id } }) => `napain ${id}`)
+  .listen(3000);
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
