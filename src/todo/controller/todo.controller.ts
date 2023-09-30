bc62398fcb3b3fadf79421ee8424b799eabb3974
FForFachriza <dimasavrian346@gmail.com>
2023-09-30 08:10:22 +0700

    [+] Change Todo Body to group

--- src/todo/controller/todo.controller.ts
+++ src/todo/controller/todo.controller.ts
@@ -1,18 +1,32 @@
 import Elysia, { NotFoundError, t } from "elysia";
 import { getTodo, getSingleTodo, addTodo } from "../service/todo.service";
 
-export const todo = new Elysia().group("/todo", (app) =>
-  app
-    .get("/", () => {
-      return getTodo();
-    })
-    .get("/:id", ({ params: { id } }) => {
-      return getSingleTodo(id);
-    })
-    .post("/", ({ body: { todo_title ,todo_categories } }) => addTodo(todo_title, todo_categories), {
-      body: t.Object({
-        todo_title: t.String({ minLength: 3 }),
-        todo_categories: t.String({ minLength: 3 }),
-      }),
-    })
+export const todo = new Elysia().group(
+  "/todo",
+  {
+    body: t.Object({
+      todo_title: t.String({ minLength: 3 }),
+      todo_categories: t.String({ minLength: 3 }),
+    }),
+  },
+  (app) =>
+    app
+      .get("/", () => {
+        return getTodo();
+      })
+      .get("/:id", ({ params: { id } }) => {
+        return getSingleTodo(id);
+      })
+      .post("/", ({ body: { todo_title, todo_categories } }) => {
+        return addTodo(todo_title, todo_categories);
+      })
+      // Todo
+      // .patch("/:id", ({ body: { todo_categories, todo_title }, params: {id} }) => {
+      //   return 
+      // })
 );
+
+// body: t.Object({
+//   todo_title: t.String({ minLength: 3 }),
+//   todo_categories: t.String({ minLength: 3 }),
+// }),
